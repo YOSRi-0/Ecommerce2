c357825266e503864cdb410c4831f3c4cb191a42
Yosri Hammadi <yosri.hammadi@gmail.com>
2021-11-26 21:58:35 +0100

    create newsletter section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Categories from './components/categories/Categories';
 import Hero from './components/hero/Hero';
 import Nav from './components/nav/Nav';
+import Newsletters from './components/newsletter/Newsletter';
 import TopProducts from './components/topProducts/TopProducts';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <TopProducts />
       <Categories />
+      <Newsletters />
     </>
 
   );
---
+++ src/components/newsletter/Newsletter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Newsletters.style.css'
+
+const Newsletters = (props) => {
+  return (
+    <section id="newsletter">
+      <div className="newsletter container">
+        <h3 className="newsletter-heading">Lorem ipsum dolor</h3>
+        <p className="newsletter-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet
+          luctus venenatis, lectus magna fringilla urna, porttitor</p>
+        <div className="newsletter__sign_up">
+          <input className="newsletter__sign_up-input" type="text" placeholder="Enter email address" />
+          <button className="btn btn-sign-up">Sign up</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Newsletters
