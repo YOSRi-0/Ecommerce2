59ac2c15ca65912f8fc46dd5a2459eb3687ecb9e
Yosri Hammadi <yosri.hammadi@gmail.com>
2021-11-26 18:19:34 +0100

    add Hero section

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Hero.style.css'
+
+const Hero = (props) => {
+  return (
+    <section id="hero">
+      <div className="hero container">
+        <div className="hero__content">
+          <h3 className="hero__content-title">
+            Lorem ipsum dolor sit
+          </h3>
+          <p className="hero__content-text">
+            Lorem ipsum dolor sit amet, 
+consectetur
+          </p>
+          <button className="btn btn-light">shop now</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
